169964625a5a21f6a4dfdedf9eec40be2e16dcbe
Cheremisinovael2 <cheremisinovael2@gmail.com>
2026-09-16 11:47:36 +0300

    fix navigation import

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-import Navigation from './components/Navigation'
+import Navigation from './components/navigation'
 
 import Home from './pages/home'
 import Catalogue from './pages/catalogue'
